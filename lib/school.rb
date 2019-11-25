a0ce694f12f3c1e399a07468230571ddaa2dea30
Joshua Nguyen-Cabral <joshycsm@gmail.com>
2019-11-25 05:38:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+class School
+  attr_accessor
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    
+  end
+  
+  def length 
+    x-
+  end
+end
